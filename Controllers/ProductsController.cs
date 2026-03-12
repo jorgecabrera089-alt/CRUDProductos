c8fe8678bb97c21baecd9d79719dacab9c19a6c8
Jorge Luis Cabrera <tu_correogmail.com>
2026-03-11 21:26:35 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUDProductos.Models;
+using System.Linq;
+
+namespace CRUDProductos.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly SampleDbContext _context;
+
+        public ProductsController(SampleDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var productos = _context.Products.ToList();
+            return View(productos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var producto = _context.Products.Find(id);
+            if (producto == null) return NotFound();
+
+            _context.Products.Remove(producto);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using CRUDProductos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDProductos.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public virtual Category? Category { get; set; }
+}
